2d2550d73f44b3323f0809705e6c61386a06ddd6
alisson-t-bucchi <alisson.bucchi@outlook.com>
2026-02-20 18:06:38 -0300

    adding faker tests

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import {test, expect} from '@playwright/test';
 import { PageManager } from '../page-objects/pageManager';
+import { faker } from '@faker-js/faker';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/');
@@ -19,10 +20,13 @@ test('Using Page Objects', async ({ page }) => {
 
 test('Using parametrized methods', async ({ page }) => {
     const pm = new PageManager(page);
+    const randomName = faker.person.fullName();
+    //const randomEmail = faker.internet.email();~
+    const randomEmail = faker.internet.email({ firstName: randomName.split(' ')[0], lastName: randomName.split(' ')[1] });
 
     await pm.navigateTo().formLayoutsPage();
     await pm.onFormLayoutsPage().submitFormWithCredentials("test@test.com", "password123", "Option 2");
-    await pm.onFormLayoutsPage().submitFormInBasicForm("Alisson B", "alisson.bucchi@test.com", true);
+    await pm.onFormLayoutsPage().submitFormInBasicForm(randomName, randomEmail, true);
     await pm.navigateTo().datepickerPage();
     await pm.onDatepickerPage().selectDatePickerFromToday(7);
     await pm.onDatepickerPage().selectDatePickerFromTodayWithRange(7, 14);
